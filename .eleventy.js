39d35b0e621b95fce6a81f4c7db3acacc0e6dfc1
Andrew Leedham <andrew.leedham@xerox.com>
2021-10-08 00:49:53 +0000

    Favicons

--- .eleventy.js
+++ .eleventy.js
@@ -63,6 +63,8 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("manifest.webmanifest");
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
